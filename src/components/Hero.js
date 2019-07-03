2fa70ec313ba49807d3f606781b6c9fb475f8bac
Chris Carr <christocarr@outlook.com>
2019-07-03 21:20:48 +0100

    Add hero and banner components

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+import BackgroundImage from 'gatsby-background-image'
 
-const Hero = () => {
+const Hero = ({ img, className, children, home }) => {
   return (
-    <div>
-      Hero image
-    </div>
+    <BackgroundImage className={className} fluid={img} home={home}>
+      {children}
+    </BackgroundImage>
   )
 }
 
-export default Hero
+export default styled(Hero)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items:center;
+  min-height: ${props => props.home ? "80vh" : "50vh" }
+  background-position: center;
+  background-size: cover;
+
+`
