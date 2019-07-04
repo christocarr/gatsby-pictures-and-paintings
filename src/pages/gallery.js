5a5ebf43afa762e19d95ea412453f31fb53b179a
Chris Carr <christocarr@outlook.com>
2019-07-04 19:56:18 +0100

    Delete comments

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
-import { graphql } from 'gatsby';
+import { graphql } from 'gatsby'
+import Paintings from '../components/Paintings'
 
 export default ({ data }) => (
   <Layout>
     <Hero img={data.image.childImageSharp.fluid} />
-  {/* <Paintings /> component goes here */} 
+    <Paintings />
   </Layout>
 )
 
-/* get contentful image instead of local image */
 export const query = graphql`
   query {
     image: file(relativePath: {eq: "gallery-hero.jpg"}) {
