9ae547c05d2b662ce333744f7070d2712a980c79
Chris Carr <christocarr@outlook.com>
2019-07-06 15:03:29 +0100

    Add footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
+
+import styles from '../css/footer.module.css'
 
 const Footer = () => {
   return (
-    <div>
-      This is the footer
-    </div>
+    <footer>
+      <p>
+        &copy; Richard Carr 2019 - All rights reserved
+      </p>
+      <ul className={styles.footerNav}>
+        <AniLink fade to="/">Home</AniLink>
+        <AniLink fade to="/gallery">Gallery</AniLink>
+        <AniLink fade to="/contact">Contact</AniLink>
+      </ul>
+    </footer>
   )
 }
 
