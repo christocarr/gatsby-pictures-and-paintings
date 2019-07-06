9403ecc8c7222ac944622d82747f8a9f6d940d86
Chris Carr <christocarr@outlook.com>
2019-07-06 14:46:17 +0100

    Add about component

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import React from 'react' 
+
+const About = () => (
+  <section>
+    <h2>About Us</h2>
+    <div>
+      <p>The artworks on offer in our gallery represents a variety of genres, executed in oils, acrylics and mixed media. All  the paintings featured on this website are original and are reasonably priced.</p>
+
+      <p>Richard Carr's gallery showcases landscapes, fantasy art, surreal art and abstract art.</p>
+
+      <p>Richard studied art in the seventies at the Pretoria School of Art in South Africa. His major subjects were painting and sculpture. While busy with his fine art studies he also qualified as an art teacher.</p>
+
+      <p>After leaving art school he taught in Pretoria for two years before leaving the classroom to become a graphic artist and photographer - subjects he had studied at art school. He never stopped painting and drawing however and, when he took an early retirement, it was the natural thing for him to decide to embark on a career as an artist.</p>
+
+      <p>Richard left South Africa in 2009 to settle in West Yorkshire. He now lives and works in Haworth.</p>
+
+      <p>Whilst in South Africa Richard took part in many group exhibitions in Johannesburg, Pretoria and Cape Town. He is presently concentrating on abstract painting and loves exploring colour, form and texture.</p>
+    </div>
+  </section>
+)
+
+export default About
