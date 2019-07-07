9754f7261d96f29d52eae2cd492d332ff8f0d089
Chris Carr <christocarr@outlook.com>
2019-07-07 12:55:55 +0100

    Add link to pictures

--- src/components/Painting.js
+++ src/components/Painting.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Img from 'gatsby-image'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
 
 import styles from '../css/painting.module.css'
 
@@ -7,10 +8,12 @@ const Painting = ({ painting }) => {
   const { title, id, slug, price, size, medium, image } = painting
 
   return (
-    <div className={styles.paintingContainer}>
-      <h3>{title}</h3>
-      <Img fluid={image.fluid} />
-    </div>
+      <div className={styles.paintingContainer}>
+        <h3>{title}</h3>
+        <Img fluid={image.fluid} />
+        <AniLink fade to="/" className={styles.btnDetails}>Details</AniLink>
+      </div>
+
   )
 }
 
