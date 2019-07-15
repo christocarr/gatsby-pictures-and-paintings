11f8ae0ba1dea47513e0d710d4e5cdbfd687dd43
Chris Carr <christocarr@outlook.com>
2019-07-15 19:58:24 +0100

    Add ribbon to painting if sold as component

---
+++ src/components/SoldRibbon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from '../css/soldRibbon.module.css'
+
+const SoldRibbon = () => {
+  return (
+    <div className={styles.ribbon}>
+      Sold
+    </div>
+  )
+}
+
+export default SoldRibbon
