3ca58434c91683b292289b919f91359eee68c604
Chris Carr <christocarr@outlook.com>
2019-07-15 19:06:41 +0100

    Add layout

--- src/pages/canceled.js
+++ src/pages/canceled.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Layout from '../components/Layout'
+import Hero from '../components/Hero'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
+
+import canceledImg from '../images/canceled_hero.jpg'
 
 const Canceled = () => {
   return (
-    <div>
-      Canceled
-    </div>
+  <Layout>
+    <Hero img={canceledImg} />
+    Success
+  </Layout>
   )
 }
 
--- src/pages/success.js
+++ src/pages/success.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
-import Banner from '../components/Banner'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
+
+import successImg from '../images/success_hero.jpg'
 
 const Success = () => {
   return (
   <Layout>
-    {/* <Hero home="true" img={data.defaultBcg.childImageSharp.fluid}>
-      <Banner title="Pictures and Paintings" />
-      <AniLink fade to="/gallery" className="link"><button className="btn-primary">Gallery</button></AniLink>
-    </Hero> */}
+    <Hero img={successImg} />
     Success
   </Layout>
   )
