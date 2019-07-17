1f67782c7acf502d097306cb8a59caaa12d8c5d7
christocarr <christocarr@outlook.com>
2019-07-17 18:52:30 +0100

    Add credit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
+import { Link } from 'gatsby'
 
 import styles from '../css/footer.module.css'
 
 const Footer = () => {
   return (
     <footer>
+      <p className={styles.credit}>Developed by <Link to="https://pictures-and-paintings.netlify.com/">Chris Carr</Link></p>
       <p className={styles.copyright}>
         &copy; Richard Carr 2019 - All rights reserved
       </p>
