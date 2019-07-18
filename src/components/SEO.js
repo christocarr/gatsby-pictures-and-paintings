1a13eb6f6a69931fc360703f1938d01193109019
Chris Carr <christocarr@outlook.com>
2019-07-18 19:24:25 +0100

    Add favicon

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
+import favicon from '../images/favicon.png'
+
 const SEO = ({ title, desc }) => {
   return (
-    <Helmet title={title}>
+    <Helmet title={title} link={{rel: 'icon', type: 'image/png', sizes: '16x16', href: `${favicon}` }}>
       <html lang="en" />
       <meta name="description" content={desc} />
     </Helmet>
