4c23661924d7ab85a9e4fb1cd33702ccb06cc11b
Chris Carr <christocarr@outlook.com>
2019-07-18 18:38:38 +0100

    Fix link to portfolio site

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
-import { Link } from 'gatsby'
 
 import styles from '../css/footer.module.css'
 
 const Footer = () => {
   return (
     <footer>
-      <p className={styles.credit}>Developed by <Link to="https://iamchriscarr.com">Chris Carr</Link></p>
+      <p className={styles.credit}>Developed by <a href="https://iamchriscarr.com">Chris Carr</a></p>
       <p className={styles.copyright}>
         &copy; Richard Carr 2019 - All rights reserved
       </p>
