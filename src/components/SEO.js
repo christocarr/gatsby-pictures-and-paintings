ecc5e658f94e0ba86e2b89e449e294714dea8ac5
Chris Carr <christocarr@outlook.com>
2019-07-18 19:29:11 +0100

    fix link data type

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -5,7 +5,7 @@ import favicon from '../images/favicon.png'
 
 const SEO = ({ title, desc }) => {
   return (
-    <Helmet title={title} link={{rel: 'icon', type: 'image/png', sizes: '16x16', href: `${favicon}` }}>
+    <Helmet title={title} link={[{rel: 'icon', type: 'image/png', sizes: '16x16', href: `${favicon}` }]}>
       <html lang="en" />
       <meta name="description" content={desc} />
     </Helmet>
